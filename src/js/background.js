759aa35ab4a7974071760145aa5a240cb59409d1
Iralitv <airyne.litvinchuk@gmail.com>
2020-05-31 17:57:20 +0300

    fix: fix load background image

--- src/js/background.js
+++ src/js/background.js
@@ -2,7 +2,7 @@ const imageContainer = document.querySelector('.img__container');
 const imageReload = document.querySelector('.header__img');
 
 const backgroundImage = async () => {
-  const city = localStorage.getItem('city').toLowerCase() || 'minsk';
+  const city = localStorage.getItem('city') || 'minsk';
   const IMAGE_API_KEY = 'd17326fa14ae234e07091f4bd4fcadf9515b23b6cab3182c7a4d4b9a25da3882';
   const imageUrl = `https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=${city}&client_id=${IMAGE_API_KEY}`;
   try {
